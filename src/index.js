5dd9f692ac339c2f14ede4e1f6214f9eaa8c42de
manavp <15manavpandey@gmail.com>
2021-07-26 12:56:32 +0530

    Create a React app and publish it to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function Tweet(){
+  return(
+    <div className = "tweet">
+      <Avatar />
+      <div className = "content">
+        <NameWithHandle /><Time />
+        <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <ReTweetButton />
+          <LikeButton />
+          <MoreOptionButton />
+        </div>
+      </div>
+      </div>
+  );
+}
+function Avatar(){
+  return(
+    <img src = "https://www.gravatar.com/avatar/nothing" className = "avatar" alt="avatar" />
+  );
+}
+function Message(){
+  return(
+    <div className = "message">This is Less than 140 character long.</div>
+  );
+}
+function NameWithHandle(){
+  return(
+    <span className="name-with-handle">
+    <span className="name">Your Name</span>
+    <span className="handle">@yourhandle</span>
+    </span>
+  );
+}
+const Time = () => (
+<span className="time">  -  3h ago</span>
 );
+const ReplyButton = () => (
+  <i className="fa fa-reply reply-button" />
+  );
+const ReTweetButton = () => (
+  <i className="fa fa-retweet retweet-button" />
+  );
+const LikeButton = () => (
+  <i className="fa fa-heart like-button" />
+  );
+const MoreOptionButton = () => (
+  <i className="fa fa-ellipsis-h more-option-button" />
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(<Tweet />,document.getElementById('root'));
